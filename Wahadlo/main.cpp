9ae71763305ed9f1cf5597379d18a136425a32de
Marek Lukasiewicz <marqus34@gmail.com>
2017-05-22 13:39:52 +0200

    Rozwiązane alfa i omega dla przykładowych danych

--- Wahadlo/main.cpp
+++ Wahadlo/main.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <cstdlib>
 #include <iostream>
@@ -6,9 +7,43 @@
 
 using namespace std;
 
-int main() {
-	cout << "Bedzie co?" << endl;
+#define N 2 //iloœæ równañ w uk³adzie
+#define ITER 110 //maksymalna iloœæ kroków ca³kowania
 
-	system("pause");
+double g = 9.81;//przyspieszenie grawitacyjne
+double l = 1;//d³ugoœæ wahad³a
+
+void rhs(double x, double y[], double out[]);
+
+int main() {
+	ofstream fout = ofstream("output.txt");
+
+	double t0 = 0;//pocz¹tkowy czas
+	double tf = 10;//koñcowy czas
+
+	double al0 = M_PI / 6;//pocz¹tkowe wychylenie
+	double om0 = 0;//pocz¹tkowa prêdkoœæ k¹towa
+
+	double h = 0.1;//krok ca³kowania
+
+	double y[ITER][N] = { 0 };//tablica wartoœci wektorów [t][n]
+
+	y[0][0] = al0;
+	y[0][1] = om0;
+
+	for (int i = 0; t0 + i*h < tf; i++) {
+		vrk4(t0 + i*h, y[i], h, N, rhs, y[i + 1]);
+	}
+
+	for (int i = 0; i < 100; i++) {
+		fout << y[i][0] << "\t" << y[i][1] << endl;
+	}
+
+	//system("pause");
 	return 0;
+}
+
+void rhs(double x, double y[], double out[]) {
+	out[0] = y[1];//omega
+	out[1] = -g / l*sin(y[0]);//alfa
 }
