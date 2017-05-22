0a7b59e6f506956465ea578979af94af57a066f5
Marek Lukasiewicz <marqus34@gmail.com>
2017-05-22 12:49:00 +0200

    Dodane definicje funkcji całkujących

---
+++ Wahadlo/main.cpp
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include "rk4.h"
+
+using namespace std;
+
+int main() {
+	cout << "Bedzie co?" << endl;
+
+	system("pause");
+	return 0;
+}
